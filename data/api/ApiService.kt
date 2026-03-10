5b1161b2f9a252297ed03c3b32bb2b62d5ad7462
AndroidOwn4898 <166979562+AndroidOwn4898@users.noreply.github.com>
2026-03-10 18:13:31 +0530

    Create ApiService interface for API endpoints

---
+++ data/api/ApiService.kt
@@ -0,0 +1,13 @@
+package com.example.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiService {
+    @GET("users/{user}")
+    fun getUser(@Path("user") user: String): Call<User>
+
+    @GET("posts")
+    fun getPosts(): Call<List<Post>>
+}
